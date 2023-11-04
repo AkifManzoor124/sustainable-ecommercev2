5a7d49f3596452ba67351b868dd43da5d11423b4
Akif <akifmanzoor124@gmail.com>
2023-11-04 19:42:54 -0400

    Uploading boilerplate for chrome extension

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,12 @@
-import { printLine } from './modules/print';
-
 console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
-printLine("Using the 'printLine' function from the Print Module");
+const dpContainer = document.getElementById('dp-container');
+
+const reactAppContainer = document.createElement('div');
+reactAppContainer.id = 'amazon-sustainable-ecommerce-rating-extension-root'; // Unique ID for your React component container
+dpContainer.appendChild(reactAppContainer);
+
+const scriptElement = document.createElement('script');
+scriptElement.src = chrome.runtime.getURL('./pages/Popup/index.jsx'); // Load your React app script
+reactAppContainer.appendChild(scriptElement);
