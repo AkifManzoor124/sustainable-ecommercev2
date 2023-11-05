6f42308a4402e8e772620771a2f1dec5d365b669
Akif <akifmanzoor124@gmail.com>
2023-11-05 06:07:50 -0500

    getting ratings from chatgpt prompt

--- src/chatgpt.js
+++ src/chatgpt.js
@@ -1,9 +1,26 @@
 
-const apiKey = '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';
+const apiKey = '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';
 
 export function getResponseFromChatGPT(message) {
+
+    return `
+    Ingredients: 2/5
+    The product does not mention the use of organic ingredients, and it includes a patented formula, which may or may not be eco-friendly.
+    Packaging: 2/5
+    The package dimensions are provided, but there is no information about the materials used. Without more details, it's unclear whether the packaging is sustainable or recyclable.
+    Certifications: 1/5
+    The product description does not mention any recognized eco-friendly certifications. It's uncertain whether the product has undergone any certification processes.
+    Sustainable Sourcing: 2/5
+    There is no information provided regarding the brand's sourcing practices or whether they responsibly and sustainably source their ingredients. Without this information, it's challenging to assess their sustainability efforts.
+    Recyclability: 2/5
+    While the package dimensions are provided, there is no explicit mention of the packaging being recyclable or if the brand offers any recycling or refill programs. More information is needed to evaluate this aspect.
+    Overall Rating: 2/5
+    While the package dimensions are provided, there is no explicit mention of the packaging being recyclable or if the brand offers any recycling or refill programs. More information is needed to evaluate this aspect.
+    Final Verdict: The product lacks sufficient information to confidently determine its eco-friendliness. It falls short in various categories, including certifications, sustainable sourcing, and recyclability.    
+    `
+
+
     console.log("hhhhhh")
-    console.log(message);
     const url = "https://api.openai.com/v1/chat/completions";
     const body = {
         model: "gpt-3.5-turbo",
@@ -30,7 +47,7 @@ export function getResponseFromChatGPT(message) {
         }
     }).then(data => {
         console.log(data);
-        return data.choices[0].text.trim();
+        return data.choices[0];
     }).catch(error => {
         console.error(error);
     });
